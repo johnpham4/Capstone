0610f67fa5f1c27d23ef01bc8ca109d5c3e135a4
quanggne <khanhquangh@gmail.com>
2026-03-20 18:35:36 +0700

    upload data

--- src/services/datasets/utils.py
+++ src/services/datasets/utils.py
@@ -11,9 +11,14 @@ def create_instruct_train_test_split(
         samples_dicts = [sample.model_dump() for sample in samples]
 
         if len(samples_dicts) > 0:
-            train_samples_dicts, test_samples_dicts = train_test_split(
-                samples_dicts, test_size=test_size, random_state=random_state
-            )
+            # Allow disabling the test split by setting test_size to 0 or None.
+            if test_size in (0, 0.0, None):
+                train_samples_dicts = samples_dicts
+                test_samples_dicts = []
+            else:
+                train_samples_dicts, test_samples_dicts = train_test_split(
+                    samples_dicts, test_size=test_size, random_state=random_state
+                )
 
             train_samples = [InstructDatasetSample(**sample_dict) for sample_dict in train_samples_dicts]
             test_samples = [InstructDatasetSample(**sample_dict) for sample_dict in test_samples_dicts]
@@ -25,5 +30,6 @@ def create_instruct_train_test_split(
         train_dataset = InstructDataset(samples=train_samples)
         test_dataset = InstructDataset(samples=test_samples)
 
-    return InstructTrainTestSplit(train=train_dataset, test=test_dataset, test_split_size=test_size)
+    effective_test_size = 0.0 if test_size in (0, 0.0, None) else float(test_size)
+    return InstructTrainTestSplit(train=train_dataset, test=test_dataset, test_split_size=effective_test_size)
 
