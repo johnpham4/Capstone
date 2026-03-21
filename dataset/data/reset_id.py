94e5cba534b64aa1cf13b292213586c7f5fa396b
johnny-lang <85885766+johnny-lang@users.noreply.github.com>
2026-03-22 01:48:55 +0700

    update

---
+++ dataset/data/reset_id.py
@@ -0,0 +1,14 @@
+import json
+
+file_path = "minh_data.json"
+
+with open(file_path, "r", encoding="utf-8") as f:
+    problems = json.load(f)
+
+for idx, prob in enumerate(problems):
+    prob["id"] = idx
+
+with open(file_path, "w", encoding="utf-8") as f:
+    json.dump(problems, f, ensure_ascii=False, indent=2)
+
+print("finish")
