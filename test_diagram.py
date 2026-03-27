68e77d5f0cfe3a8e87da243ca7e2db5db8e84867
XuanNhi183 <volexuannhi1803@gmail.com>
2026-03-27 19:20:12 +0700

    filter problem from 221 to the end

--- test_diagram.py
+++ test_diagram.py
@@ -65,7 +65,7 @@ def test_single_problem(instruction, dsl_answer, output_path):
         print(f"Instructions count: {len(builder.instructions)}")
         # print(f"Instructions: {builder.instructions}")
         opts = {
-            'epochs': 1500,
+            'epochs': 3000,
             'n_tries': 3,
             'eps': 1e-6,
             'seed': 42,
